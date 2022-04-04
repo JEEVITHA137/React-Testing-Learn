3aecf71479e6ea96de1e151d6fe23cda99362209
Jeevitha Venkatesan <jeevitha.venkatesan@surveysparrow.com>
2022-04-04 22:14:57 +0530

    Button Test

---
+++ src/_test/button.test.js
@@ -0,0 +1,45 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import ButtonInput from '../button';
+
+const mockTest = jest.fn();
+
+
+describe('Button', () => {
+  test('Check Initial State', () => {
+    const value = '+';
+    const currentSymbol = ''
+    render(<ButtonInput key={value} symbol={value} currentSymbol={currentSymbol} currentSymbolChange={mockTest}/>);
+    const buttonElement = screen.getByRole("button", { name: value });
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveClass('btn-dark');
+  });
+
+  test('Check Function Called', () => {
+    const value = '+';
+    const currentSymbol = ''
+    render(<ButtonInput key={value} symbol={value} currentSymbol={currentSymbol} currentSymbolChange={mockTest}/>);
+    const buttonElement = screen.getByRole("button", { name: value });
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveClass('btn-dark');
+    fireEvent.click(buttonElement);
+    expect(mockTest).toBeCalledWith('+');
+  });
+
+  test('Check Current Button Symbol Class', () => {
+    const value = '+';
+    const currentSymbol = '+'
+    render(<ButtonInput key={value} symbol={value} currentSymbol={currentSymbol} currentSymbolChange={mockTest}/>);
+    const buttonElement = screen.getByRole("button", { name: value });
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveClass('btn-success');
+  });
+
+  test('Check Clear Button', () => {
+    const value = 'Clear';
+    const currentSymbol = '';
+    render(<ButtonInput key={value} symbol={value} currentSymbol={currentSymbol} currentSymbolChange={mockTest}/>);
+    const buttonElement = screen.getByRole("button", { name: value });
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveClass('btn-primary');
+  });
+});
